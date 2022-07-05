6eaf35acd494f85d3444e4894bb5b3557b377578
Tommy Rogers <hrodgeri9@gmail.com>
2022-07-05 10:07:20 -0400

    more items from chapter

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
+var palette = []color.Color{color.White, color.RGBA{255, 0, 0, 255}}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//fmt.Println("Hello, World!")
+	lissajous(os.Stdout)
+	//echo()
+	//dup()
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms unitsxx
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+
+}
+
+func echo() {
+	sep := " - "
+	for index, arg := range os.Args[0:] {
+		fmt.Println(strconv.Itoa(index) + sep + arg)
+	}
+}
+
+func dup() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		countLinesForEachFile(files, counts)
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLinesForEachFile(files []string, counts map[string]int) {
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			continue
+		}
+		countLines(f, counts)
+		f.Close()
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
 }
