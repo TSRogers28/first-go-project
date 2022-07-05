95d6d99b816b77d51fc391e7cfe8a0a67788e567
Tommy Rogers <hrodgeri9@gmail.com>
2022-07-05 10:21:20 -0400

    adding fetch

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,10 @@ import (
 	"image/color"
 	"image/gif"
 	"io"
+	"io/ioutil"
 	"math"
 	"math/rand"
+	"net/http"
 	"os"
 	"strconv"
 )
@@ -22,9 +24,10 @@ const (
 
 func main() {
 	//fmt.Println("Hello, World!")
-	lissajous(os.Stdout)
+	//lissajous(os.Stdout)
 	//echo()
 	//dup()
+	fetch()
 }
 
 func lissajous(out io.Writer) {
@@ -94,5 +97,22 @@ func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+	}
+}
+
+func fetch() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
 	}
 }
